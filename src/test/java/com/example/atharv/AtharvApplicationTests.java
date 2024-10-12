f2d5f5a4ad9b35baf104a566792eeec5f75d38b0
Atharvaher <aheratharv2806@gmail.com>
2024-10-12 17:17:16 +0530

    ok

---
+++ src/test/java/com/example/atharv/AtharvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atharv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtharvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
